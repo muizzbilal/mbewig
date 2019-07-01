32b991e503b1fe695f74343452c4a8cdb61f7208
Shiekh Muizz <muizzbilal@gmail.com>
2019-07-01 10:21:57 +0500

    mouizzwfff

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,7 +1,11 @@
 module.exports = {
-    MongoURI: process.env.MONGO_URI,
-    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
-    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+    MongoURI: process.env.MongoURI,
+    GoogleClientID: process.env.GoogleClientID,
+    GoogleClientSecret: process.env.GoogleClientSecret,
+    FacebookClientID:process.env.FacebookClientID,
+    FacebookClientSecret:process.env.FacebookClientSecret,
+    InstagramClientID:process.env.InstagramClientID,
+    InstagramClientSecret:process.env.InstagramClientSecret
 }
 
 
